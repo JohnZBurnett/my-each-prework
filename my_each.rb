03232cd3d97a5779c9c67370c34345be9d31356a
JohnZBurnett <tuninator@gmail.com>
2018-04-13 16:50:58 -0400

    Wrote my version of each.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  i = 0
+  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end
+  arr
 end
+
+test_arr = [1, 2, 3, 4]
+my_each(test_arr) do |item|
+  puts item
+end
+puts "We're done!"
